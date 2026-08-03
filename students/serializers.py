ff5d763a35380ae4d5131b54cf4a893b6b332fd4
fayzullayevvvv <ozodbekfayzullayev1220@gmail.com>
2026-08-03 14:37:51 +0500

    feat(student): student model, services, views and initial migrations added

---
+++ students/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from .models import Student
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    owner_username = serializers.CharField(source='owner.username', read_only=True)
+
+    class Meta:
+        model = Student
+        fields = [
+            'id', 'owner', 'owner_username', 'first_name', 'last_name',
+            'phone_number', 'passport_number', 'status', 'balance',
+            'frozen_at', 'archived_at', 'groups'
+        ]
+        read_only_fields = ['status', 'balance', 'frozen_at', 'archived_at']
+
+
+class StudentCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['owner', 'first_name', 'last_name', 'phone_number', 'passport_number', 'groups']
+
+
+class TransferStudentSerizalizer(serializers.Serializer):
+    current_group_id = serializers.IntegerField()
+    target_group_id = serializers.IntegerField()
